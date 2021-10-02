3f7af40445db874b393e58e8e674fea4534067c0
Mitch Samaniego <mitch.sam7@gmail.com>
2021-10-02 00:58:58 -0500

    commit inicial

---
+++ test-bankapp-ios-endcom/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  test-bankapp-ios-endcom
+//
+//  Created by Mitch Samaniego on 02/10/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+    
+}
---
+++ test-bankapp-ios-endcom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-bankapp-ios-endcom
+//
+//  Created by Mitch Samaniego on 02/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
